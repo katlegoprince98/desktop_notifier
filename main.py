9d7cf62de64c245a39c038b95dfa284467125ac1
katlegoprince98 <CynicsTech1998@gmail.com>
2023-02-20 18:08:25 -0800

    final

---
+++ main.py
@@ -0,0 +1,8 @@
+from plyer import notification
+
+notification.notify(title="TAKE A BREAK FROM CODING",
+                    message="Taking small breaks is essential",
+                    app_icon='icon/not.ico',
+                    timeout=60
+
+)
